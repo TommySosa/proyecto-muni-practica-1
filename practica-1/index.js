04106b75786a62fef6ab999e3b348be9f6f9d368
TommySosa <tomas.facundo.sosa@gmail.com>
2023-05-23 20:54:24 -0300

    punto-3

--- practica-1/index.js
+++ practica-1/index.js
@@ -1,3 +1,21 @@
 const scores = [10,9,5,7,3,3,8,5];
 
-const outstandingScores = scores.filter((score)=> score > 7)
+const outstandingScores = scores.filter(score => score > 7)
+
+const players = ['Messi', 'Di Maria', 'A. Gomez', 'De Paul', 'Pezzella', 'Almada', 'Paredes','Acuña']
+
+
+const outstandingPlayers = outstandingScores.map(player => players[scores.indexOf(player)])
+//const outstandingPlayers = outstandingScores.map(index => scores.indexOf(index))
+
+
+
+
+
+
+
+
+
+console.log('scores: ' + scores);
+console.log('outstandingScores: ' + outstandingScores);
+console.log(outstandingPlayers);
