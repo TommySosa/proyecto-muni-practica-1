5594f702bb01fc4fb24ba80d4fb7f48dd149fb22
TommySosa <tomas.facundo.sosa@gmail.com>
2023-05-23 23:20:00 -0300

    punto-6

--- practica-1/index.js
+++ practica-1/index.js
@@ -1,28 +1,36 @@
-const scores = [10,9,5,7,3,3,8,5];
+function evalPerfomance(scores,players){
+    console.log('scores: ' + scores);
 
-const outstandingScores = scores.filter(score => score > 7)
+    const outstandingScores = scores.filter(score => score > 7)
+
+    const outstandingPlayers = outstandingScores.map(player => players[scores.indexOf(player)])
+
+    const indices = outstandingScores.map(score => scores.indexOf(score))
+
+    outstandingScores.forEach(() =>{
+        scores.sort((a,b) => b-a)
+        scores.splice(indices,1)
+    })
+     
+    const regularPlayers = players.filter((player, index) => indices.indexOf(index) === -1);
+    
+    console.log('outstandingScores: ' + outstandingScores);
+    console.log('outstandingPlayers: ' + outstandingPlayers);
+    console.log('scores nuevos : ' + scores);
+    console.log('regularPlayers: ' + regularPlayers);
+}
+
+const scores = [10,9,5,7,3,3,8,5];
 
 const players = ['Messi', 'Di Maria', 'A. Gomez', 'De Paul', 'Pezzella', 'Almada', 'Paredes','Acuña']
 
-
-const outstandingPlayers = outstandingScores.map(player => players[scores.indexOf(player)])
-
-const indices = outstandingScores.map(score => scores.indexOf(score))
-
-outstandingScores.forEach(() =>{
-    scores.sort((a,b) => b-a)
-    scores.splice(indices,1)
-})
- 
-const regularPlayers = players.filter((player, index) => indices.indexOf(index) === -1);
+evalPerfomance(scores,players);
 
 
 
 
 
-console.log('scores: ' + scores);
-console.log('outstandingScores: ' + outstandingScores);
-console.log('outstandingPlayers: ' + outstandingPlayers);
-console.log(indices);
-console.log('scores nuevos : ' + scores);
-console.log('regularPlayers: ' + regularPlayers);
+
+
+
+
